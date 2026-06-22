d74dec78d78e69f604919aa26cbdd67e0409dfd2
Mario Medoni <mamed@mac.comp.clients.local>
2026-06-22 13:43:16 +0200

    init cookiecutter project

---
+++ src/labram_and_xai/train.py
@@ -0,0 +1,10 @@
+from labram_and_xai.model import Model
+from labram_and_xai.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
